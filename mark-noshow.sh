9a655df90e7a6bbe7fed2cac0f7064bd58586e80
tdobson <hello@tdobson.net>
2021-11-16 14:55:59 +0000

    fixed bugs

--- mark-noshow.sh
+++ mark-noshow.sh
@@ -7,10 +7,10 @@ id=$1
 # take argument of event ID and make all the orders in it attended and also completed
 
 #update meta field
-mysql -u root --password=`cat bitnami_application_password` --database=bitnami_wordpress -se "UPDATE wp_postmeta as t, (SELECT order_id  FROM wp_order_product_customer_lookup where order_id=$id AND status='wc-processing' AND cc_attendance='pending') as temp SET meta_value='noshow' WHERE temp.order_id = t.post_id AND meta_key='cc_attendance' AND meta_value='pending';" > /tmp/file
+mysql -u root --password=`cat ~/bitnami_application_password` --database=bitnami_wordpress -se "UPDATE wp_postmeta as t, (SELECT order_id  FROM wp_order_product_customer_lookup where order_id=$id AND status='wc-processing' AND cc_attendance='pending') as temp SET meta_value='noshow' WHERE temp.order_id = t.post_id AND meta_key='cc_attendance' AND meta_value='pending';" > /tmp/file
 
 # set update order status
-mysql -u root --password=`cat bitnami_application_password` --database=bitnami_wordpress -se "SELECT order_id  FROM wp_order_product_customer_lookup where order_id=$id AND status='wc-processing';" > /tmp/file
+mysql -u root --password=`cat ~/bitnami_application_password` --database=bitnami_wordpress -se "SELECT order_id  FROM wp_order_product_customer_lookup where order_id=$id AND status='wc-processing';" > /tmp/file
 for i in `cat /tmp/file`; do sudo wp wc shop_order update $i --status="completed" --user=1 && echo $i; done
 
 #clean up
