75db4a7e20ff064d7800ba6b376346e863769617
tdobson <hello@tdobson.net>
2022-02-13 16:35:47 +0000

    adapted no show to fix bug

--- mark-noshow.sh
+++ mark-noshow.sh
@@ -11,7 +11,7 @@ mysql -u root --password=`cat ~/bitnami_application_password` --database=bitnami
 
 # set update order status
 mysql -u root --password=`cat ~/bitnami_application_password` --database=bitnami_wordpress -se "SELECT order_id  FROM wp_order_product_customer_lookup where order_id=$id AND status='wc-processing';" > /tmp/file
-for i in `cat /tmp/file`; do sudo wp wc shop_order update $i --status="completed" --user=1 && echo $i; done
+sudo wp wc shop_order update $id --status="completed" --user=1
 
 #clean up
 rm /tmp/file
