5d92c81c43d9a7f1c35ea1355e1bf170f95f503f
tdobson <hello@tdobson.net>
2022-02-13 16:41:19 +0000

    updated to fit automatewoo scehma

--- mark-volunteer-head-chef.sh
+++ mark-volunteer-head-chef.sh
@@ -7,7 +7,7 @@ id=$1
 # take argument of event ID and make all the orders in it attended and also completed
 
 #update meta field
-mysql -u root --password=`cat ~/bitnami_application_password` --database=bitnami_wordpress -se "UPDATE wp_postmeta as t, (SELECT order_id  FROM wp_order_product_customer_lookup where order_id=$id AND status='wc-processing' AND cc_attendance='pending') as temp SET meta_value='head-chef' WHERE temp.order_id = t.post_id AND meta_key='cc_volunteer';" 
+mysql -u root --password=`cat ~/bitnami_application_password` --database=bitnami_wordpress -se "UPDATE wp_postmeta as t, (SELECT order_id  FROM wp_order_product_customer_lookup where order_id=$id AND status='wc-processing' AND cc_attendance='pending') as temp SET meta_value='head_chef' WHERE temp.order_id = t.post_id AND meta_key='cc_volunteer';" 
 
 # set update order status
 sudo wp wc shop_order update $id --status="on-hold" --user=1
