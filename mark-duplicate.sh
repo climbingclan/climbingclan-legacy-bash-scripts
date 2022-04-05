48a89f24d8fd6e29bc0ee0811327803ea9e87b2c
tdobson <hello@tdobson.net>
2022-04-05 22:03:16 +0000

    removed error case

--- mark-duplicate.sh
+++ mark-duplicate.sh
@@ -6,11 +6,10 @@ id=$1
 
 
 #update meta field
-mysql -u root --password=`cat ~/bitnami_application_password` --database=bitnami_wordpress -se "UPDATE wp_postmeta as t, (SELECT order_id  FROM wp_order_product_customer_lookup where order_id=$id AND status='wc-processing' AND cc_attendance='pending') as temp SET meta_value='duplicate' WHERE temp.order_id = t.post_id AND meta_key='cc_attendance' AND meta_value='pending';" > /tmp/file
+mysql -u root --password=`cat ~/bitnami_application_password` --database=bitnami_wordpress -se "UPDATE wp_postmeta as t, (SELECT order_id  FROM wp_order_product_customer_lookup where order_id=$id) as temp SET meta_value='duplicate' WHERE temp.order_id = t.post_id AND meta_key='cc_attendance';" > /tmp/file
 
 # set update order status
-mysql -u root --password=`cat ~/bitnami_application_password` --database=bitnami_wordpress -se "SELECT order_id  FROM wp_order_product_customer_lookup where order_id=$id AND status='wc-processing';" > /tmp/file
-for i in `cat /tmp/file`; do sudo wp wc shop_order update $i --status="completed" --user=1 && echo $i; done
+sudo wp wc shop_order update $id --status="completed" --user=1 && echo $id
 
 #clean up
 rm /tmp/file
