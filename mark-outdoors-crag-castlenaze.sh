ee42efbefb0d4f2052b178b07009ae60731b174e
tdobson <hello@tdobson.net>
2022-05-04 22:36:19 +0000

    fixed and adapted stuff

--- mark-outdoors-crag-castlenaze.sh
+++ mark-outdoors-crag-castlenaze.sh
@@ -8,7 +8,7 @@ id=$1
 # take argument of event ID and make all the orders in it attended and also completed
 
 #update meta field
-mysql -u root --password=`cat ~/bitnami_application_password` --database=bitnami_wordpress -se "UPDATE wp_postmeta as t, (SELECT order_id  FROM wp_order_product_customer_lookup where order_id=$id AND status='wc-processing' AND cc_attendance='pending') as temp SET meta_value='castlenaze' WHERE temp.order_id = t.post_id AND meta_key='cc_location' AND meta_value='pending';" 
+mysql -u root --password=`cat ~/bitnami_application_password` --database=bitnami_wordpress -se "UPDATE wp_postmeta as t, (SELECT order_id  FROM wp_order_product_customer_lookup where order_id=$id AND NOT status='wc-completed' ) as temp SET meta_value='castlenaze' WHERE temp.order_id = t.post_id AND meta_key='cc_outdoor_location';" 
 
 # set update order status
 sudo wp wc shop_order update $1 --status="on-hold" --user=1
